b35cc2a377708c1ffaf40781f82e9d91d81bc3d4
Aryxnnn <akadam2204@gmail.com>
2026-02-14 08:29:00 +0530

    pandas

--- imp_series.py
+++ imp_series.py
@@ -1,2 +1,40 @@
 import numpy as py
 import pandas as pd
+'''
+#astype
+series.astype('int16')
+
+#between
+series.between(x,y)
+
+#clip
+series.clip(x,y) convert values lower than x to x and values lower than y to y
+
+#drop_duplicates
+series.drop_duplicates(keep='last')
+
+#duplicated
+series.duplicated()
+
+#isnull()
+series.isnull()
+
+#dropna
+series.dropna()
+
+#fillna
+series.fillna()
+
+#isin()
+series.isin([])
+
+#apply
+series.apply( lamda x : x.split()[0].upper())
+
+#copy
+
+series.copy()
+
+
+
+'''
