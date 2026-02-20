8d26248fde08429bdb143637315aede0bd04ea91
Aryxnnn <akadam2204@gmail.com>
2026-02-20 20:10:58 +0530

    groupby

---
+++ Groupby.py
@@ -0,0 +1,90 @@
+''' //here dataset is movie//
+Aryan=movies.groupby('Genre')
+Aryan.sum() // can apply more aggregate functions on groupby
+
+//find the top 3 genres by total earning
+movies.groupby('Genre')['Gross'].sum().sort_values(ascending=False).head(3)
+
+//find genre with highest imdb rating
+movies.groupby('Genre')['IMDB'].mean().sort_values(ascending=False).head(3)
+
+// most voted director
+movies.groupby('Director')['Votes].sort_values(ascending=False).head()
+
+// no of movies by actor
+->movies['star1'].value_counts() //easyyy
+movies.groupby('Star1')['Series'].count().sort_values(ascending=False).head()
+
+len(movies.groupby('Genre'))
+
+movies.groupby('Genre').size()
+
+genres=movies.groupby('Genre')
+genres.first()
+
+genres.nth(x)
+
+genres.get_group('Horror')
+
+//object.group returns dictionary 
+
+genres.describe()
+//returns all info
+
+genres.sample //returns random data
+'''
+
+'''
+//passing dict 
+genres,agg(
+    {
+        'Runtime' : 'mean',
+        'IMDB : 'mean' ,
+        'Gross ': 'sum'
+        'Metascore' : 'min'
+    }
+)
+//passing list
+genres.agg (['min' 'max' 'mean'])
+
+//combining both
+genres,agg(
+    {
+        'Runtime' : ['mean','min' ]
+        'IMDB :'mean' ,
+        'Gross ': ['sum','max']
+        'Metascore' : 'min'
+    }
+)
+
+//looping for groups
+df=p
+for group,data in genres:
+df=df.append(data[data['IMDB']== data['IMDB'].max())
+df
+
+//apply functions
+->genre.apply(min) 
+
+def foo(group):
+return group['serial'].str.startswith('a').sum()
+genre.apply(foo)
+
+
+def ranking(group):
+group['rankingg']= group['imdb'].rank(ascending=False)
+return group
+genre.apply(ranking)
+
+duo = movies.groupby(['Director','Star1'])
+duo.size()
+duo.get_group('Amir khan' ,'Anmole')
+
+duo['Gross'].sum().sort_values(ascending=False).head()
+
+movies.groupby(['star1','genre'])['Metascore'].mean().reset_index().sort_values(ascending=false).head()
+
+duo.agg(['min' 'max' 'mean'])
+
+
+'''
